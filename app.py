473c601db41320f158d110c383c61b11860c2b16
Abuton <alaroabubakarolayemi@yahoo.com>
2021-08-08 21:04:24 +0100

    cost by year metrics 4 private instituition added

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
 # run docker build --tag=college-history
 
+# import dependencies
 from pyspark.sql import SparkSession, DataFrame, Window
 import pyspark.sql.functions as F
 
-
+# read all csv data in data/
 def read_college_date(spark: SparkSession, data_location: str = 'data/*.csv') -> DataFrame:
     df = spark.read.csv(data_location, header='true')
     return df
 
-
+# add the source file column to df
 def add_source_file(df: DataFrame) -> DataFrame:
     df = df.withColumn('source_file', F.input_file_name())
     return df
 
-
+# select only important columns
 def select_and_filter(df: DataFrame) -> DataFrame:
-    df = df.select('NPT4_PUB', 'STABBR', 'source_file')
+    df = df.select('NPT4_PUB', 'NPT4_PRIV', 'STABBR', 'source_file')
     df = df.where(F.col('NPT4_PUB') != 'NULL') # if we don't have cost information, throw it out.
     return df
 
-
+# extract year from the newly added column 'source_file'
 def pull_year_from_file_name(df: DataFrame) -> DataFrame:
     # pull out the year that can be found in the file name between "MERGED" and "_"
     df = df.withColumn('year',
@@ -27,13 +28,16 @@ def pull_year_from_file_name(df: DataFrame) -> DataFrame:
                         'MERGED(.*)_', 1), 1, 4))
     return df
 
-
-def pull_cost_by_year_metrics(df: DataFrame) -> DataFrame:
-    results = df.groupby('year').agg(F.avg('NPT4_PUB')).alias('avg_cost')
+# get cost per year on public instituition
+def pull_cost_by_year_metrics_public(df: DataFrame) -> DataFrame:
+    results = df.groupby('year').agg(F.avg('NPT4_PUB')).alias('avg_cost_public_instituiion')
     results.sort(F.col('year').desc()).show()
 
 
-
+# get cost per year on private instituition
+def pull_cost_by_year_metrics_private(df: DataFrame) -> DataFrame:
+    results = df.groupby('year').agg(F.avg('NPT4_PRIV')).alias('avg_cost_private_instituiion')
+    results.sort(F.col('year').desc()).show()
 
 def pull_most_expensive_states_for_college(df: DataFrame):
     df = df.filter(F.col('year') == 2019)
@@ -52,7 +56,8 @@ def main():
     df = add_source_file(df)
     df = select_and_filter(df)
     df = pull_year_from_file_name(df)
-    pull_cost_by_year_metrics(df)
+    pull_cost_by_year_metrics_public(df)
+    pull_cost_by_year_metrics_private(df)
     pull_most_expensive_states_for_college(df)
 
 
